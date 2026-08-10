6c49baa787d3b5f426990c284abe2a041f7afbc2
GooRingX <gooringx@gmail.com>
2026-08-10 09:52:59 +0800

    update

--- lib/app/utils/app_utils.dart
+++ lib/app/utils/app_utils.dart
@@ -24,7 +24,7 @@ abstract final class AppUtils {
   }
 
   static String getBuildinVersion() {
-    return "1.0.28.1405";
+    return "1.0.28.1406";
   }
 
   static DateTime getBuildinVersionDate() {
