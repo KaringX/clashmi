672ca75618032566381b7065d601d0afaaf0cbfd
GooRingX <gooringx@gmail.com>
2026-09-24 09:46:16 +0800

    update

--- lib/app/utils/stacktrace_utils.dart
+++ lib/app/utils/stacktrace_utils.dart
@@ -1,13 +1,10 @@
 class StackTraceUtils {
   static String trim(StackTrace stackTrace, int? depth) {
-    List<String> st = stackTrace.toString().split('\n');
-    for (int i = 0; i < st.length; i++) {
-      if (depth != null && i >= depth) {
-        // Trim the rest of the stack trace
-        st.removeRange(i, st.length);
-        break;
-      }
-    }
-    return st.join("\n");
+    String stack = stackTrace
+        .toString()
+        .split("\n")
+        .take(depth ?? 5)
+        .join("\n");
+    return stack;
   }
 }
